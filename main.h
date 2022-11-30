efac3688312551eccbd474523cb84131581d73e3
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2022-11-30 20:44:59 +0300

    Initial commit

---
+++ main.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <gl/glew.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+#include <iostream>
+#include <vector>
+#include <corecrt_math_defines.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
